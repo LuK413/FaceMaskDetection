b4888437f37d0900ffb43de412e821994b30c1a5
Kai Lu <kaisiulu@gmail.com>
2022-01-09 23:31:59 -0500

    moved model weights to different path

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 st.title('Face Detection App')
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp2/weights/best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 model.eval()
 
 st.markdown(
